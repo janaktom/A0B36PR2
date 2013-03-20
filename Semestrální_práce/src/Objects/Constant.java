d8c4d6dedf3bb0f3ee84e0f18deb5a69fee391b2
TOMHP <TOMHP@TOMHP-HP>
2013-03-20 20:49:23 +0100

    pridana trida Constant

---
+++ Semestrální_práce/src/Objects/Constant.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objects;
+
+/**
+ *
+ * @author TOMHP
+ */
+public class Constant extends Expr {
+
+    @Override
+    double evaluate() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    Expr derive(char var) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    Expr simplify() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
